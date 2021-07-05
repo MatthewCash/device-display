f61e2dd986700552b8387dc1211b2199e7b6af74
Matthew_Cash <matthew@matthew-cash.com>
2021-07-05 12:05:49 -0700

    Merge changes

--- src/sections/Devices.vue
+++ src/sections/Devices.vue
@@ -1,15 +1,9 @@
 <template>
-<<<<<<< HEAD:src/sections/Info.vue
-    <div class="info">
-        <h1 class="info-title">Info</h1>
-        <hr class="info-separator" />
-        <h2 class="disconnected">Disconnected from Device Controller</h2>
-        <h3 v-if="devices.length === 0">No Devices Detected</h3>
-=======
     <div class="devices">
         <h1 class="devices-title">Devices</h1>
         <hr class="devices-separator" />
->>>>>>> 1cc772112ff6bd780ef7b55c19b97f8bfb2447c6:src/sections/Devices.vue
+        <h2 class="disconnected">Disconnected from Device Controller</h2>
+        <h3 v-if="devices.length === 0">No Devices Detected</h3>
         <div
             class="device-container"
             v-for="device of devices"
@@ -18,9 +12,9 @@
         >
             <div
                 class="device-status"
-                :class="{ 'device-enabled': device.status }"
+                :class="{ 'devices-enabled': device.status }"
             >
-                {{ device.status ? "On" : "Off" }}
+                {{ device.status ? 'On' : 'Off' }}
             </div>
             <div class="device-info">
                 <span class="device-name">{{ device.name }}</span>
@@ -30,11 +24,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "vue";
-import { Device, devices, setDevice } from "../devices";
+import { defineComponent, computed } from 'vue';
+import { Device, devices, setDevice } from '../devices';
 
 export default defineComponent({
-    name: "devices",
+    name: 'devices',
     setup: () => {
         const sortedDevices = computed(() =>
             devices.sort((a, b) => a.name.localeCompare(b.name))
@@ -45,18 +39,13 @@ export default defineComponent({
     methods: {
         toggleDevice(device: Device) {
             setDevice(device.id, !device.status);
-        },
-    },
+        }
+    }
 });
 </script>
 
-<<<<<<< HEAD:src/sections/Info.vue
-<style lang="scss" >
-.info {
-=======
-<style lang="scss" scoped>
+<style lang="scss">
 .devices {
->>>>>>> 1cc772112ff6bd780ef7b55c19b97f8bfb2447c6:src/sections/Devices.vue
     display: flex;
     flex-direction: column;
     align-items: center;
